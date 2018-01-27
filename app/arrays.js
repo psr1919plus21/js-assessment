08b704136e6bb6e805b1e4225023c797cb34d835
Filipp Gaponenko <psr1919plus21@gmail.com>
2018-01-27 22:09:59 +0300

    :white_check_mark: Add array tests.

--- app/arrays.js
+++ app/arrays.js
@@ -1,20 +1,34 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.arraysAnswers = {
-  indexOf: function(arr, item) {
-
+  indexOf: function(arr, itemForSearch) {
+    let itemIndex = -1;
+    arr.find((item, index) => {
+      if (item === itemForSearch) {
+        itemIndex = index;
+        return true;
+      }
+    });
+    return itemIndex;
   },
 
   sum: function(arr) {
-
+    return arr.reduce((acc, next) => {
+      return acc + next;
+    }, 0);
   },
 
-  remove: function(arr, item) {
-
+  remove: function(arr, itemForRemove) {
+    return arr.filter((item) => item !== itemForRemove);
   },
 
-  removeWithoutCopy: function(arr, item) {
-
+  removeWithoutCopy: function(arr, itemForRemove) {
+    for (let index = arr.length; index; index--) {
+      if (arr[index] === itemForRemove) {
+        arr.splice(index, 1);
+      }
+    }
+    return arr;
   },
 
   append: function(arr, item) {
